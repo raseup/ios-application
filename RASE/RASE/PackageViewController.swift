0236fdc545c7f8c5a1caadc6a0670c0e3d4351c8
Samuel Beaulieu <sambeau@bu.edu>
2017-06-14 14:47:03 -0400

    Starting to make the package page into a dynamic table of cells so packages can be added and removed as needed and content can be changed from backend.

---
+++ RASE/RASE/PackageViewController.swift
@@ -0,0 +1,100 @@
+//
+//  AthleteViewController.swift
+//  RASE
+//
+//  Created by Sam Beaulieu on 6/14/17.
+//  Copyright © 2017 RASE. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class PackageTableViewController: UITableViewController {
+    
+    // MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Load any saved notes
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        DispatchQueue.main.async{
+            self.tableView.reloadData()
+        }
+        print("Appeared!")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "PackageViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? PackageViewCell else {
+            fatalError("The dequeued cell is not an instance of PackageViewCell.")
+        }
+        
+        // Fetches appropriate note
+        let note = notes[indexPath.row]
+        
+        cell.titleLabel.text = note.title
+        
+        return cell
+    }
+    
+    
+    
+    // Override to support conditional editing of the table view.
+  /*  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+ */
+    
+    
+    
+    // Override to support editing the table view.
+   /* override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }*/
+    
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+}
+
