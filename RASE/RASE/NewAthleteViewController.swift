76810b6de943eb3c46df035fa7596d047db39edf
Samuel Beaulieu <sambeau@bu.edu>
2017-06-16 16:20:38 -0400

    Working on skills page. Got pickerwheel to work, needs a lot more work though.

--- RASE/RASE/NewAthleteViewController.swift
+++ RASE/RASE/NewAthleteViewController.swift
@@ -21,23 +21,35 @@ class NewAthleteViewController: UIViewController, UIPickerViewDelegate, UIPicker
     @IBOutlet weak var videoLabel: UILabel!
     @IBOutlet weak var videoTextField: UILabel!
     @IBOutlet weak var checkoutButton: UIButton!
-    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var pickerAge: UIPickerView!
+    @IBOutlet weak var pickerSkills: UIPickerView!
+    @IBOutlet weak var pickerPosition: UIPickerView!
+    @IBOutlet weak var scrollConstraint: NSLayoutConstraint!
     
     var ageList: [String] = [String]()
     var skillList = ["Introductive Level", "Fundemental Level", "Intermediate Level", "Advanced Level", "Professional Level"]
     var positionList = ["Point Guard", "Shooting Guard", "Small Forward", "Power Forward", "Center", "I do not have a position"]
     
-    var pickerSource: [String] = [String]()
     var currentTextField: UITextField? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentTextField = shootingTextField
         
-        self.picker.delegate = self
-        self.picker.dataSource = self
+        self.pickerAge.delegate = self
+        self.pickerAge.dataSource = self
+        self.pickerSkills.delegate = self
+        self.pickerSkills.dataSource = self
+        self.pickerPosition.delegate = self
+        self.pickerPosition.dataSource = self
         self.ageTextField.delegate = self
         self.positionTextField.delegate = self
+        self.shootingTextField.delegate = self
+        self.ballHandlingTextField.delegate = self
+        self.finishingTextField.delegate = self
+        self.passingTextField.delegate = self
+        self.defenseTextField.delegate = self
         
         for val in 4...75
         {
@@ -56,39 +68,82 @@ class NewAthleteViewController: UIViewController, UIPickerViewDelegate, UIPicker
     }
     
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-        return self.pickerSource.count
+        
+        if pickerView.tag == 1
+        {
+            return self.ageList.count
+        }
+        else if pickerView.tag == 2
+        {
+            return self.positionList.count
+        }
+        else
+        {
+            return self.skillList.count
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
-        self.view.endEditing(true)
-        return self.pickerSource[row]
-        
+        if pickerView.tag == 1
+        {
+            return self.ageList[row]
+        }
+        else if pickerView.tag == 2
+        {
+            return self.positionList[row]
+        }
+        else
+        {
+            return self.skillList[row]
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-        self.currentTextField!.text = self.pickerSource[row]
-        self.picker.isHidden = true
-        
+        if pickerView.tag == 1
+        {
+            self.ageTextField.text = self.ageList[row]
+            self.scrollConstraint.constant = 0
+            self.pickerAge.isHidden = true
+        }
+        else if pickerView.tag == 2
+        {
+            self.positionTextField.text = self.positionList[row]
+            self.scrollConstraint.constant = 0
+            self.pickerPosition.isHidden = true
+        }
+        else
+        {
+            self.currentTextField!.text = self.skillList[row]
+            self.scrollConstraint.constant = 0
+            self.pickerSkills.isHidden = true
+        }
     }
     
     @IBAction func ageBeginEditing(_ sender: UITextField) {
-        
-        self.currentTextField = self.ageTextField
-        self.pickerSource = self.ageList
-        self.picker.isHidden = false
-        self.currentTextField!.endEditing(true)
+        self.pickerAge.isHidden = false
+        self.scrollConstraint.constant = -175
+        self.ageTextField.endEditing(true)
     }
     
     @IBAction func positionBeginEdition(_ sender: UITextField) {
-        self.currentTextField = self.positionTextField
-        self.pickerSource = self.positionList
-        self.picker.isHidden = true
-        self.currentTextField!.endEditing(true)
-        
-        print(picker.isHidden)
+        self.pickerPosition.isHidden = false
+        self.scrollConstraint.constant = -175
+        self.positionTextField.endEditing(true)
     }
     
+    @IBAction func skillsBeginEditing(_ sender: UITextField) {
+        self.pickerSkills.isHidden = false
+        self.scrollConstraint.constant = -175
+        sender.endEditing(true)
+        currentTextField! = sender
+    }
     
+    @IBAction func shootingBeginEditing(_ sender: UITextField) {
+        self.pickerSkills.isHidden = false
+        self.scrollConstraint.constant = -175
+        shootingTextField.endEditing(true)
+        currentTextField! = shootingTextField
+    }
 }
