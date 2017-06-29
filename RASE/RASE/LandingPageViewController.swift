0df41d8a568eebc34ecd3111b566c7fdc23139b3
Samuel Beaulieu <sambeau@bu.edu>
2017-06-29 12:02:09 -0400

    Added page based landing screen and updated log in page

---
+++ RASE/RASE/LandingPageViewController.swift
@@ -0,0 +1,89 @@
+//
+//  LandingPageViewController.swift
+//  RASE
+//
+//  Created by Sam Beaulieu on 6/29/17.
+//  Copyright © 2017 RASE. All rights reserved.
+//
+
+import UIKit
+
+private(set) var orderedViewControllers: [UIViewController] = {
+    return [UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Green"),
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Red"),
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Blue")]
+}()
+
+class LandingPageViewController: UIPageViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension LandingPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex-1
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex+1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
+            return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+}
